049798816e3486a8c2d8758abfe7f8ab838a7c7f
Romain Séailles <r.seailles@gmail.com>
2019-10-18 12:29:15 +0200

    Ajout de fonctions pour plot le plan affine à f en M

--- code.py
+++ code.py
@@ -5,14 +5,31 @@ import matplotlib.pyplot as plt
 def f(x, y):
     return np.power(x,2) + np.power(y,2)
 
+def grad(f):
+    """Retourne une fonction donnant le gradient de f"""
+    h = 10**(-5)
+    return lambda x,y : np.array((f(x+h,y)-f(x,y), f(x,y+h)-f(x,y)))/h
+
+def creerPlan(f, x0, y0):
+    """Retourne une fonction donnant le plan tangent en x0, y0 de f"""
+    gradFEnM = grad(f)(x0,y0)
+    return lambda x,y : gradFEnM[0]*(x-x0) + gradFEnM[1]*(y-y0) + f(x0,y0)
 
 domain_x = np.linspace(-5, 5)   
 domain_y = np.linspace(-5, 5)
 
 X, Y = np.meshgrid(domain_x, domain_y)
-Z = f(X, Y)
+Zf = f(X, Y)
+
+x0,y0 = 2,2
+z0 = f(x0,y0)
+planEnM = creerPlan(f, x0, y0)
+Zplan = planEnM(X,Y)
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.plot_surface(X, Y, Z, cmap='winter')
-#plt.show()
+ax.plot_surface(X, Y, Zf, cmap='winter')
+ax.plot_surface(X, Y, Zplan)
+
+#ax.quiver3D(x0,y0,z0,gradf(x0,y0)[0], gradf(x0,y0)[1], 0, color="red")
+plt.show()
